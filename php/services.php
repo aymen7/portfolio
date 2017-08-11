dc6872df908636ce8f5d11a40facc0e5d090235b
aymen7 <aymenokay@gmail.com>
2017-08-11 12:10:27 +0100

    adding pic to the services.php
    making the pic's frame

--- php/services.php
+++ php/services.php
@@ -1,8 +1,23 @@
 <div class="container-fluid" id="services-div">
     <div class="row" id="services-div-title">
         <h1><b>What can I do for you</b></h1>
+    </div><!-- end of the title div-->
+    <div class="row container-fluid" id="services-content">
+        <div class="col-lg-2 col-md-2 col-lg-offset-0 container-fluid" id="pic1-frame">
+            <div id="pic1" class="row">
 
-    </div>
+            </div><!-- end of the pic1 div-->
+        </div><!-- end of the pic1-frame div-->
+        <div class="col-lg-2 col-md-2 col-lg-offset-1 container-fluid" id="pic2-frame">
+            <div id="pic2" class="row">
 
+            </div><!-- end of the pic2 div-->
+        </div><!-- end of the pic2-frame div-->
+        <div class="col-lg-2 col-md-2 col-lg-offset-1 container-fluid" id="pic3-frame">
+            <div id="pic3" class="row">
+
+            </div><!-- end of the pic3 div-->
+        </div><!-- end of the pic3-frame div-->
+    </div><!--end of the content div -->
 
 </div><!-- end od the services div-->
